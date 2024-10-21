5fb2051dcdd46c55840b52c13728e2cebb8ed823
filippinoepro <filippinoepro@gmail.com>
2024-10-21 23:28:05 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// import {laCantineDAgatheApi} from './api';
+import axios from 'axios';
+
+function App() {
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/users');
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error fetching data', error);
+    }
+  };
+
+  return (
+      <button className='bg-red-600' onClick={fetchData}>fetch data in console</button>
+  )
+}
+
+export default App
---
+++ src/api.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+// Utilisation de la variable d'environnement
+const API_URL = process.env.REACT_APP_API_URL;
+
+export const laCantineDAgatheApi = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+});
+
+// Intercepteur pour gérer les erreurs globales
+laCantineDAgatheApi.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.setItem('persist:rootPersist', JSON.stringify({}));
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
